03a0c95f32d85a334406139915b5a894b2ed6e62
dgriego <daniel@jackrabbitsystems.com>
2016-08-21 23:03:22 -0600

    adds search functionality

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -1,9 +1,9 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 before do
-  @table_of_contents = File.readlines("data/toc.txt")
+  @table_of_contents = File.readlines('data/toc.txt')
 end
 
 get '/' do
@@ -22,14 +22,85 @@ get '/chapter/:number' do
   erb :chapter
 end
 
+get '/search' do
+  @results = chapters_matching(params['query'])
+
+  erb :search
+end
+
 not_found do
   redirect '/'
 end
 
-helpers do
-  def in_paragraphs(text)
-    text.split("\n\n").map do |line|
-      "<p>#{line}</p>"
-    end.join
+# TA SOLUTION
+def each_chapter(&block)
+  @table_of_contents.each_with_index do |title, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, title, contents
   end
 end
+
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, title, chapter|
+    matches = {}
+
+    chapter.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+
+    results << {number: number, title: title, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").each_with_index.map do |line, index|
+      "<p id='paragraph_#{index}'>#{line}</p>"
+    end.join
+  end
+
+  def highlight(paragraph, term)
+    paragraph.gsub(term, "<strong>#{term}</strong>")
+  end
+end
+
+# MY SOLUTIONS
+# def chapters_matching(query)
+#   results = []
+
+#   return results unless query
+
+#   count = 1
+#   loop do
+#     chapter = File.read("data/chp#{count}.txt").downcase
+#     query = params['query'].downcase
+
+#     if chapter.include?(query)
+#       results << @table_of_contents[count - 1]
+#     end
+
+#     count += 1
+#     break if count == 13
+#   end
+
+#   results
+# end
+
+# def paragraphs_matching(query, chapter)
+#   results = []
+
+#   chapter.split("\n\n").each_with_index do |paragraph, index|
+#     if paragraph.include?(query)
+#       results << { number: index + 1, content: paragraph }
+#     end
+#   end
+
+#   results
+# end
